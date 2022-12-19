5b2f61b87ffc5ee2537f33d191ac6fc4484196dc
Grégori Hebert <96366701+gre-hebert@users.noreply.github.com>
2022-12-19 00:35:16 -0300

    Update index.py

--- index.py
+++ index.py
@@ -3,7 +3,11 @@ import random
 
 pygame.init()
 pygame.display.set_caption('Meu jogo')
-tela = pygame.display.set_mode((800,600))
+tela = pygame.display.set_mode((0,0), pygame.FULLSCREEN)
 
 while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+
+            quit()
     pass
